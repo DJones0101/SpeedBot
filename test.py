d3553f80207cee424f639cd4c7444a00ad4baec8
DJones0101 <darius.jones1018@gmail.com>
2019-04-06 23:37:29 -0700

    fixed gpio, added to readme

--- test.py
+++ test.py
@@ -21,14 +21,14 @@ left =  20
 
 # setup 
 gpio.setmode(gpio.BCM)
-gpio.setup(right,gpio.IN)
-gpio.setup(left,gpio.IN)
+gpio.setup(right,gpio.IN,pull_up_down=gpio.PUD_UP)
+gpio.setup(left,gpio.IN,pull_up_down=gpio.PUD_UP)
 gpio.setup(ena[0],gpio.OUT)
 gpio.setup(ena[1],gpio.OUT)
 gpio.setup(enb[0],gpio.OUT)
 gpio.setup(enb[1],gpio.OUT)
 gpio.setup(in1,gpio.OUT)
-gpio.setup(in3,gpio.OUT)
+gpio.setup(in2,gpio.OUT)
 gpio.setup(in3,gpio.OUT)
 gpio.setup(in4,gpio.OUT)
 gpio.output(ena[0],True)
@@ -37,31 +37,45 @@ gpio.output(enb[0],True)
 gpio.output(enb[1],True)
 gpio.setwarnings(False)
 
+lefEncoder = gpio.input(left)
+rightEncoder = gpio.input(right)
+
 
 def forward(tf):
 
 	gpio.output(in1,True)
-	gpio.output(in3,False)
+	gpio.output(in2,False)
 	gpio.output(in3,True)
 	gpio.output(in4,False)
 	time.sleep(tf)
-	gpio.cleanup()
+	
 
 def backward(tf):
 	gpio.output(in1,False)
-	gpio.output(in3,True)
+	gpio.output(in2,True)
 	gpio.output(in3,False)
 	gpio.output(in4,True)
 	time.sleep(tf)
-	gpio.cleanup()
+	
+def rightTurn(tf):
+	gpio.output(in1,True)
+	gpio.output(in2,False)
+	gpio.output(in3,False)
+	gpio.output(in4,True)
+	time.sleep(tf)
+
+def leftTurn(tf):
+	gpio.output(in1,False)
+	gpio.output(in2,True)
+	gpio.output(in3,True)
+	gpio.output(in4,False)
+	time.sleep(tf)
+
 
 
 
 
 if __name__ == '__main__':
 	
-		
-	#print( "left encoder: %d" %gpio.input(left))
-	#print( "right encoder: %d" %gpio.input(right))
 	
-	forward(4)
+	gpio.cleanup()
