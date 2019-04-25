efe43d04347459130b29ee7780b2d97f11e05e15
Colin Hinton <colinlewishinton@gmail.com>
2019-04-25 11:37:13 -0700

    Revert "Small math changes"
    This reverts commit 8554ba0c35f10ed8cedc7b4637ce521c9bdb7490.

--- M7_Code/main.py
+++ M7_Code/main.py
@@ -12,27 +12,6 @@ from pyb import UART
 # example see the BLE-Shield driver.
 uart = UART(3, 9600)
 
-# Variables from Github 
-BINARY_VIEW = True
-GRAYSCALE_THRESHOLD = (240, 255)
-MAG_THRESHOLD = 4
-THETA_GAIN = 40.0
-RHO_GAIN = -1.0
-P_GAIN = 0.7
-I_GAIN = 0.0
-I_MIN = -0.0
-I_MAX = 0.0
-D_GAIN = 0.1
-STEERING_SERVO_INDEX = 0
-
-sensor.reset()
-sensor.set_pixformat(sensor.GRAYSCALE)
-sensor.set_framesize(sensor.QQQVGA)
-sensor.set_vflip(True)
-sensor.set_hmirror(True)
-sensor.skip_frames(time = 2000)
-clock = time.clock()
-
 while(True):
     uart.write("Hello World!\n")
     time.sleep(1000)
