e2ef86372e22590315ff503c1ebc109884211ffc
DJones0101 <darius.jones1018@gmail.com>
2019-05-09 16:20:18 -0700

    stuff

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ in2 = 22
 in3 = 23
 in4 = 24
 
-# setup 
+# setup
 gpio.setmode(gpio.BCM)
 gpio.setup(in1, gpio.OUT)
 gpio.setup(in2, gpio.OUT)
@@ -26,12 +26,15 @@ os.system("gpio mode 23 pwm")
 os.system("gpio mode 26 pwm")
 os.system("gpio pwm-ms")
 os.system("gpio pwmr 4000")
-os.system("gpio pwmr 4095")
+os.system("gpio pwmr 4095") 
+
 
 error_count = 0
 
 
 def forward(tf, duty1, duty2):
+
+    
     command1 = "gpio pwm 23 %d" % duty1
     command2 = "gpio pwm 26 %d" % duty2
 
@@ -42,8 +45,10 @@ def forward(tf, duty1, duty2):
     gpio.output(in2, gpio.LOW)
     gpio.output(in3, gpio.HIGH)
     gpio.output(in4, gpio.LOW)
-    time.sleep(tf)
+    time.sleep(1)
 
+
+    
 
 def stop(tf):
     command1 = "gpio pwm 23 %d" % 0
@@ -78,9 +83,9 @@ def get_msg():
         msg = []
         count = 0 
         ser.flushInput()
-        while True:    
+        while True:
             msg.append(ser.read(1).decode())
-            time.sleep(0.01)
+           # time.sleep(0.01)
             if msg[count] == '\n':
                 #print(msg)
                 break
@@ -89,42 +94,79 @@ def get_msg():
         return "".join(msg)
 
 
+
 def split_directions(string_input):
     list_results = string_input.split(",")
     direction = ""
     magnitude = 0
+    # print(string_input)
+    #print(list_results)
+    # print(type(list_results[0]))
+    # print(type(list_results[1]))
     # do we need dummy variable for /n or /r
     # should it be != 4 
     if len(list_results) != 3:
         # error case
-        direction = "ERROR: list size = %d" % (len(list_results))
+        #direction = "ERROR: list size = %d" % (len(list_results))
+        
         return direction, magnitude
     else:
         direction = list_results[0]
         magnitude = list_results[1]
+        # print(direction)
+        # print(magnitude)
         return direction, magnitude
 
 
 if __name__ == '__main__':
+    stop(1)
     try:
         while True:
             commands = get_msg()
-            print(commands)
-            direction, magnitude = split_directions(commands)
-            print(direction, " ", magnitude)
-            if direction == "left" or direction == "right" or direction == "straight":
-                forward(1, 4000, 4000)
-                # previous left block, change back after testing
-            # elif direction == "right":
-            #     forward(1, 4000, magnitude * 4000)
-            # elif direction == "straight":
-            #     forward(1, 4000, 4000)
-            else:
-                if error_count >= 3:
-                    rightTurn(1, 2000)
-                    stop(1)
-                    error_count = 0
-                else:
-                    error_count += 1
+
+            #print(commands)
+            # direction, magnitude = split_directions(commands)
+            # print(direction, " ", magnitude)
+            # time.sleep(.1)
+
+            length = len(commands)
+            string_index = length - 12
+            magnitude = 0
+            direction = "s"
+
+            if string_index >= 0:
+                commands = commands[string_index:]
+                direction = commands[0]
+                magnitude = int(commands[4:len(commands)-2])
+                magnitude = magnitude
+                print(direction)
+                print(magnitude)
+
+            if direction == "r":
+                magnitude = 0 - magnitude
+
+            k = 225
+            v = 3000 
+            l = .15
+            v_r = min(v + (k*magnitude*l)/2, 4000)
+            v_l = .9 * min(v - (k*magnitude*l)/2, 4000)
+            print("k: %f, v: %d, v_r %d: . v_l %d: , " %(k, v, v_r, v_l))
+
+            v_r = max(v_r, 0)
+            v_l = max(v_l, 0)
+            # vleft_offset = 3000
+            # vright_offset = 3350
+
+            forward(.25,v_l,v_r)
+            # forward(.25,3000,3350)
+
+
+            # else:
+            #     if error_count >= 3:
+            #         rightTurn(1, 2000)
+            #         stop(1)
+            #         error_count = 0
+            #     else:
+            #         error_count += 1
     except KeyboardInterrupt:
         gpio.cleanup()
